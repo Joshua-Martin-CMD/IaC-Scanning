b79f85cacb63f019e962ee05fdb5dff2ba72775f
Joshua.Martin <joshua.martin@cmd.com.au>
2023-02-06 21:31:05 +1100

    deployment of EC2 and SecurityGroups

--- CDK-TS-Demo/lib/EC2_stack.ts
+++ CDK-TS-Demo/lib/EC2_stack.ts
@@ -12,10 +12,56 @@ export class CdkTsDemoStack extends cdk.Stack {
     super(scope, id, props);
 
     const proj_vpcId = cmd_cdk.env_config[environment]['Vpc_Id']
+    const proj_ami = cmd_cdk.env_config[environment].instance['ami-id']
 
-    const vpc = ec2.Vpc.fromLookup(this,
+    const vpclookup = ec2.Vpc.fromLookup(this,
       'VPC-Lookup', {
-        vpcId: proj_vpcId
+        vpcId: proj_vpcId // lookup from cdk.json for VPCID in the dev,uat or prod account
       })
+    
+      const securityGroup = new ec2.SecurityGroup(
+        this,
+        'IaC-Demo-sg',
+        {
+          vpc: vpclookup,
+          allowAllOutbound: true, // will let your instance send outboud traffic
+          securityGroupName: 'IaC-Demo-sg',
+        }
+      )
+  
+      // lets use the security group to allow inbound traffic on specific ports
+      securityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(22),
+        'Allows SSH access from Internet'
+      )
+  
+      securityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(80),
+        'Allows HTTP access from Internet'
+      )
+  
+      securityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(443),
+        'Allows HTTPS access from Internet'
+      )
+    
+    const instance = new ec2.Instance(this, 'Linux-Instance', {
+      vpc: vpclookup,
+      securityGroup: securityGroup,
+      instanceName: 'IaC-Demo_instance',
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.C6G,
+        ec2.InstanceSize.MICRO
+        ),
+        machineImage: ec2.MachineImage.latestAmazonLinux({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        })
+      })
+    
+
+    
   }
 }
